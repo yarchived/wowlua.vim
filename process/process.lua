52c29646d1fa9313f20a75f7b25c68528d1dfceb
yaroot <yaroot@gmail.com>
2010-10-13 17:06:56 +0800

    add 4.0.1 new apis and bit funcs

--- process/process.lua
+++ process/process.lua
@@ -1,57 +1,70 @@
 
 --{{{
+local apis = {}
+
 local function open(path, mode)
     local f = io.open(path, mode or 'r')
     if(not f ) then
-        error('Cannot open file ' .. rawname)
+        error('Cannot open file ' .. path or '')
     else
         return f
+    end
+end
+
+local function valid(str)
+    return str and str~='' and str~=' '
+end
+
+local function findDuplicate(t, s)
+    for i, v in ipairs(t) do
+        if(v) == s then
+            return true
+        end
     end
 end
 --}}}}
 
 --{{{
-local function go(rawname, chunkname, reg, type_str)
-    local raw_file = open(rawname)
+local function parse(raw, re, type_str)
+    local rawstrings = {}
 
     -- get the api out
-    local rawstrings = {}
+    local raw_file = open(raw)
     for line in raw_file:lines() do
-        if(line~='' and line ~= ' ') then
-            local str = line:match(reg)
-            if(str and str~='' and str~=' ') then
-                rawstrings[str] = true
+        if(valid(line)) then
+            local str = line:match(re)
+            if(valid(str) and not findDuplicate(rawstrings, str)) then
+                table.insert(rawstrings, str)
             end
         end
     end
     raw_file:close() -- close file
 
-    -- write to the file
-    local chunk_file = open(chunkname, 'r')
-    for k, v in pairs(rawstrings) do
-        chunk_file:write(string.format('syn keyword %s %s\n', type_str, k))
+    table.sort(rawstrings)
+
+    -- add them together
+    for i, v in ipairs(rawstrings) do
+        table.insert(apis, string.format('syn keyword %s %s\n', type_str, v))
     end
 
-    chunk_file:flush()
-    chunk_file:close()
+    table.insert(apis, '\n') -- make a empty line
 end
 --}}}
 
---{{{
-go('raw_api', 'chunk_api', '^([a-z,A-Z_]+)', 'luaWoWAPI')
-go('raw_event', 'chunk_event', '^([A-Z_]+)', 'luaWoWEvent')
-go('raw_widget', 'chunk_widget', ':(%w+)%(', 'luaWoWWidget')
+--{{{ go go rock
+table.insert(apis, '\n')
+parse('raw_api', '^([a-z,A-Z_]+)', 'luaWoWAPI')
+parse('raw_event', '^([A-Z_]+)', 'luaWoWEvent')
+parse('raw_widget', ':(%w+)%(', 'luaWoWWidget')
 --}}}
 
---{{{ cat the file together
-local cmd = [[
-
-]]
-
--- os run
-
-
-
+--{{{ write to the file
+local file = open('chunk.vim', 'w')
+for i, v in ipairs(apis) do
+    file:write(v)
+end
+file:flush()
+file:close()
 --}}}
 
 
